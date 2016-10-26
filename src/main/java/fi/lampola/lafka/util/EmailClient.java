a9c08695e7a28df56d6f94fb07958d70fe4cd953
Markus Lampola <lampola@hotmail.com>
2016-10-26 15:09:25 +0300

    Email settings can be overridden by environment variables.

--- src/main/java/fi/lampola/lafka/util/EmailClient.java
+++ src/main/java/fi/lampola/lafka/util/EmailClient.java
@@ -6,6 +6,7 @@
 package fi.lampola.lafka.util;
 
 import fi.lampola.lafka.domain.Henkilo;
+import java.util.Map;
 import java.util.Properties;
 import javax.mail.Message.RecipientType;
 import javax.mail.PasswordAuthentication;
@@ -35,6 +36,9 @@ public class EmailClient {
     @Value("${smtp.from}")
     private String from;
 
+    @Value("${smtp.auth}") // true or false
+    private String auth;
+
     @Value("${smtp.username}")
     private String username;
 
@@ -43,6 +47,33 @@ public class EmailClient {
 
     public void send(Henkilo to, String link) {
 
+        Map<String, String> env = System.getenv();
+        
+        // Override by environment variables
+        if (env.containsValue("SMTP_HOST")) {
+            host = env.get("SMTP_HOST");
+        }
+        
+        if (env.containsValue("SMTP_PORT")) {
+            port = env.get("SMTP_PORT");
+        }
+        
+        if (env.containsValue("SMTP_FROM")) {
+            from = env.get("SMTP_FROM");
+        }
+        
+        if (env.containsValue("SMTP_AUTH")) {
+            auth = env.get("SMTP_AUTH");
+        }
+        
+        if (env.containsValue("SMTP_USERNAME")) {
+            username = env.get("SMTP_USERNAME");
+        }
+        
+        if (env.containsValue("SMTP_PASSWORD")) {
+            password = env.get("SMTP_PASSWORD");
+        }
+        
         Properties props = new Properties();
 
         if (port.equals("587") || port.equals("465")) {
@@ -56,7 +87,12 @@ public class EmailClient {
             props.put("mail.smtp.ssl.trust", "*");
         }
 
-        props.put("mail.smtp.auth", "true");
+        if (Boolean.parseBoolean(auth)) {
+            props.put("mail.smtp.auth", "true");
+        } else {
+            props.put("mail.smtp.auth", "false");
+        }
+        
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.port", port);
 
@@ -73,7 +109,7 @@ public class EmailClient {
 
             message.setFrom(new InternetAddress(from));
             message.setRecipients(RecipientType.TO, InternetAddress.parse(to.getEmail(), false));
-            message.setSubject("Password reset link");
+            message.setSubject("Salasanan asetus");
             message.setText("Aseta uusi salasana tällä linkillä:\n\n" + link);
 
             Transport.send(message);
